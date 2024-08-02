b1862d6c767ace9ea857f759bdb24f2b8ff41ed4
Neo <wtzd8645@gmail.com>
2024-08-03 04:30:02 +0800

    feat(memory): Add Align function to MemoryUtil

---
+++ Memory/MemoryUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace Blanketmen.Hypnos.Memory
+{
+    public static class MemoryUtil
+    {
+        public static int Align(int pos, int align)
+        {
+            // TODO: return (n + 7) & static_cast<size_t>(-8); // Align n to next multiple of 8 (from Hacker's Delight, Chapter 3.)
+            // NOTE: -offset & (Align - 1) equal to (Align - (offset % Align)) % Align;
+            return pos + (-pos & (align - 1));
+        }
+    }
+}
