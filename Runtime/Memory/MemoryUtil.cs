dc3e034832eb12d321d667dadd1ca811ce8d2cdf
Neo <wtzd8645@gmail.com>
2024-11-07 03:48:05 +0800

    feat: Add AlignDown function to MemoryUtil

--- Runtime/Memory/MemoryUtil.cs
+++ Runtime/Memory/MemoryUtil.cs
@@ -4,12 +4,24 @@ namespace Blanketmen.Hypnos.Memory
 {
     public static class MemoryUtil
     {
+        // TODO: return (n + 7) & static_cast<size_t>(-8); // Align n to next multiple of 8 (from Hacker's Delight, Chapter 3.)
+
+        /// <summary>
+        /// Aligns the specified value up to the nearest multiple of the alignment.
+        /// </summary>
+        /// <param name="val">The value to align.</param>
+        /// <param name="align">The alignment value, which must be a power of two.</param>
+        /// <returns>The smallest integer greater than or equal to <paramref name="val"/> that is a multiple of <paramref name="align"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Align(int pos, int align)
-        {
-            // TODO: return (n + 7) & static_cast<size_t>(-8); // Align n to next multiple of 8 (from Hacker's Delight, Chapter 3.)
-            // NOTE: -offset & (Align - 1) equal to (Align - (offset % Align)) % Align;
-            return pos + (-pos & (align - 1));
-        }
+        public static int AlignUp(int val, int align) => val + (-val & (align - 1));
+
+        /// <summary>
+        /// Aligns the specified value down to the nearest multiple of the alignment.
+        /// </summary>
+        /// <param name="val">The value to align.</param>
+        /// <param name="align">The alignment value, which must be a power of two.</param>
+        /// <returns>The largest integer less than or equal to <paramref name="val"/> that is a multiple of <paramref name="align"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int AlignDown(int val, int align) => val & ~(align - 1);
     }
 }
